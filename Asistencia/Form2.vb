3a115d7f0a5c08536636e0e65973c56f48c5789d
Jhon Fernandez Galarza <jhon_63639@hotmail.com>
2013-06-26 16:46:50 -0500

    asd

---
+++ Asistencia/Form2.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form2
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Db_asistenciaDataSet1.VW_Listar' table. You can move, or remove it, as needed.
+        Me.VW_ListarTableAdapter.Fill(Me.Db_asistenciaDataSet1.VW_Listar)
+
+    End Sub
+End Class
